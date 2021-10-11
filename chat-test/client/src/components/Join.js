e2ce532540b8df6fefaa622a07da14ad16c8f52b
pm-g-15 <senbhavin@gmail.com>
2021-10-11 16:19:18 +0530

    added changes in class

--- chat-test/client/src/components/Join.js
+++ chat-test/client/src/components/Join.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { default as socket } from "../socket/ws";
-
+import "../scss/app.scss";
 const Join = () => {
   const [nickname, setNickname] = useState();
   const history = useHistory();
@@ -16,22 +16,20 @@ const Join = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen bg-gray-100">
-      <div className="grid place-items-center my-auto">
-        <h1 className="font-bold text-4xl pb-10 text-gray-900 antialiased">
-          Chat App 🦜
-        </h1>
-        <form className="flex w-full max-w-sm space-x-3 justify-center">
+    <div className="chat_wrapper">
+      <div className="chat_inner">
+        <h1 className="abc">Chat App 🦜</h1>
+        <form className="form_inner">
           <div className="relative ">
             <input
               type="text"
               onChange={(e) => setNickname(e.target.value)}
-              className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent"
+              className="input_inner"
               placeholder="Nickname"
             />
           </div>
           <button
-            className="flex-shrink-0 bg-green-400 text-gray-800 text-base font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
+            className="button_inner"
             onClick={() => {
               submitNickname();
               handleOnClick();
